d133390c309200979e5a9f697636fda2179ae2ab
sinan özlen <sinanozlen1@gmail.com>
2024-12-10 22:23:50 +0300

    main page updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,14 @@ module.exports = {
     },
     extend: {
       colors: {
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
+        brightBlue: 'hsl(210, 88%, 59%)', // Canlı mavi
+        brightBlueLight: 'hsl(210, 88%, 69%)', // Daha açık mavi
+        brightBlueSupLight: 'hsl(210, 88%, 95%)', // Çok açık mavi
+        darkBlue: 'hsl(228, 39%, 23%)', // Koyu mavi
+        darkGrayishBlue: 'hsl(227, 12%, 61%)', // Griye çalan mavi
+        veryDarkBlue: 'hsl(233, 12%, 13%)', // Çok koyu mavi
+        veryPaleBlue: 'hsl(210, 100%, 96%)', // Soluk mavi
+        veryLightGray: 'hsl(0, 0%, 98%)', // Çok açık gri
       },
     },
   },
