c6ac9c978fb1c269ebe835ebe30ab78bc6ffc75d
sinan özlen <sinanozlen1@gmail.com>
2024-12-11 16:46:37 +0300

    navbar eklendi

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
-const btn = document.getElementById('menu-btn');
-const nav = document.getElementById('menu');
+// Genel işlev için fonksiyon
+function handleMenuHover(menuButtonId, dropdownId) {
+  const menuButton = document.getElementById(menuButtonId);
+  const dropdownMenu = document.getElementById(dropdownId);
+  let timeoutId;
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open');
-  nav.classList.toggle('flex');
-  nav.classList.toggle('hidden');
-});
+  menuButton.addEventListener("mouseenter", () => {
+    clearTimeout(timeoutId);
+    dropdownMenu.classList.remove("hidden");
+    // Diğer menüler kapanacak
+    document.querySelectorAll('.dropdown-menu').forEach((menu) => {
+      if (menu !== dropdownMenu) {
+        menu.classList.add("hidden");
+      }
+    });
+  });
+
+  menuButton.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(() => {
+      dropdownMenu.classList.add("hidden");
+    }, 300); // Menü 300ms sonra kapanır
+  });
+
+  dropdownMenu.addEventListener("mouseenter", () => {
+    clearTimeout(timeoutId);
+  });
+
+  dropdownMenu.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(() => {
+      dropdownMenu.classList.add("hidden");
+    }, 300); // Menü 300ms sonra kapanır
+  });
+}
+
+// Menüleri başlat
+handleMenuHover('menuKurumsal', 'dropdownKurumsal');
+handleMenuHover('menuHizmetler', 'dropdownHizmetler');
+handleMenuHover('menuUygulamalar', 'dropdownUygulamalar');
+handleMenuHover('doubleDropdownButton', 'doubleDropdown'); // Yeni menü için
